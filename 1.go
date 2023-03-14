0b0f5eb4c4580aa420f3de19ca9047cbbe69126e
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-03-14 10:34:00 -0300

    Create 1.go

---
+++ 1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var id uint
+	var peso float64
+	fmt.Print("Qual é o seu nome? ")
+	fmt.Scan(&name)
+	fmt.Print("Qual a sua idede? ")
+	fmt.Scan(&id)
+	fmt.Print("Qual seu peso? ")
+	fmt.Scan(&peso)
+	fmt.Println("Olá", name, "co")
+}
